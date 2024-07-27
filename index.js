ee81811f6ec0d2901522fec9e3c8f7dba5f92668
Marsden Mathews Odhiambo <marsdenlwero@gmail.com>
2024-07-27 08:41:34 +0300

    Lab completed

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat() {
+    const newCats = [...cats, "Broom"];
+    return newCats;
+}
+
+function prependCat() {
+    const nCats = ["Arnold", ...cats];
+    return nCats;
+}
+function removeLastCat() {
+    const rCat = cats.slice(0, cats.length - 1);
+    return rCat;
+}
+
+function removeFirstCat() {
+    const remCat = cats.slice(1);
+    return remCat;
+}
